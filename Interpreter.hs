9fbf9eddf54399866098e1f6bfc4b56135c5a382
danidelllo <danik.kozirev@gmail.com>
2022-05-19 02:54:55 +0300

    Improvements

--- Interpreter.hs
+++ Interpreter.hs
@@ -3,10 +3,40 @@ module Interpreter where
 import AbsCalc
 
 
-interpret :: Exp -> Integer 
-interpret x = case x of 
-  EAdd exp0 exp -> interpret exp0 + interpret exp 
-  ESub exp0 exp -> interpret exp0 - interpret exp 
-  EMul exp0 exp -> interpret exp0 * interpret exp 
-  EDiv exp0 exp -> interpret exp0 `div` interpret exp
-  EInt n -> n
+runprogram :: Program -> IO String
+runprogram (PDef x) = do
+    runstatement x
+    return "1"
+
+runstatement :: [Stm] -> IO ()
+runstatement (x:xs) = case x of
+  SExp y -> do
+    runexpression y
+    putStr ""
+    runstatement xs
+  PExp y -> do
+    result <- runexpression y
+    putStrLn $ show result
+    runstatement xs
+runstatement [] = putStr ""
+
+runexpression :: Exp -> IO Integer
+runexpression x = case x of 
+  EAdd exp0 exp -> do
+      e0 <- runexpression exp0
+      e1 <- runexpression exp
+      return $ e0 + e1
+  ESub exp0 exp -> do
+      e0 <- runexpression exp0
+      e1 <- runexpression exp
+      return $ e0 - e1
+  EMul exp0 exp -> do
+      e0 <- runexpression exp0
+      e1 <- runexpression exp
+      return $ e0 * e1
+  EDiv exp0 exp -> do
+      e0 <- runexpression exp0
+      e1 <- runexpression exp
+      return $ e0 `div` e1
+  EInt n -> do
+      return $ n
